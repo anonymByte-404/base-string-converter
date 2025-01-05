564d41aabf906e09d857f60d20b239e40f8a4e90
anonymByte-404 <an0nymnx@outlook.com>
2025-01-04 20:34:54 -0500

    refactor(main.js): replace readline with inquirer for improved user prompts

--- src/main.js
+++ src/main.js
@@ -1,7 +1,43 @@
-const readline = require("readline")
+/**
+ *  Note: This application is still in development. Further enhancements will be added.
+ */
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+import inquirer from 'inquirer'
 
+const baseChoices = Array.from({ length: 63 }, (_, i) => `Base ${i + 2}`)
+
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'stringOrBase',
+            message: 'Select your conversion type below:',
+            choices: ['String', 'Base']
+        }
+    ])
+    .then((answers) => {
+        if (answers.stringOrBase === 'String') {
+            inquirer
+                .prompt([
+                    {
+                        type: 'list',
+                        name: 'stringBase',
+                        message: 'Choose a base for string conversion:',
+                        choices: baseChoices
+                    }
+                ])
+        } else if (answers.stringOrBase === 'Base') {
+            inquirer
+                .prompt([
+                    {
+                        type: 'list',
+                        name: 'baseChoice',
+                        message: 'Choose a base or string conversion:',
+                        choices: ['String', ...baseChoices]
+                    }
+                ])
+        }
+    })
+    .catch((error) => {
+        console.log('Error', error)
+    })
