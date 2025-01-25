1f7fbceeddc63516e132be4f267f3c4df4a66e47
anonymByte-404 <an0nymnx@outlook.com>
2025-01-24 23:04:04 -0500

    feat: Add JavaScript files for handling web page functionality (main, history, converter)

---
+++ public/scripts/converter.js
@@ -0,0 +1,5 @@
+// converter.js
+
+// This file will contain the JavaScript logic for performing string and base conversions
+// It will handle user input, conversion logic, and displaying the result.
+// Additional functionality will be added here later to perform conversions and update the UI.
---
+++ public/scripts/main.js
@@ -0,0 +1,4 @@
+// main.js
+
+// This file contains the main logic for the application.
+// It handles initialization, event listeners, and user interactions.
