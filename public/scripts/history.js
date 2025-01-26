55782fe8367dfa13008be06daf16f42d12d39cc7
anonymByte-404 <an0nymnx@outlook.com>
2025-01-25 22:15:45 -0500

    refactor: clean up unnecessary files

--- public/scripts/history.js
+++
@@ -1,5 +0,0 @@
-// history.js
-
-// This file will contain the JavaScript logic for handling the Conversion History
-// It will include functions to display, search, and clear the conversion history.
-// More code will be added here later to handle dynamic interactions with the history.
