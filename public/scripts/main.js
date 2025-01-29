87d270c1542e52f4aea8128ba272609f85af1f4e
anonymByte-404 <an0nymnx@outlook.com>
2025-01-28 22:21:32 -0500

    refactor: rename feature-contents to feature-content in main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,9 @@
-const featureContents = document.querySelectorAll('.feature-contents')
+const featureContent = document.querySelectorAll('.feature-content')
 
-featureContents.forEach(feature => {
+featureContent.forEach(feature => {
   feature.addEventListener('mouseover', (event) => {
     event.preventDefault()
-    featureContents.forEach(fc => fc.classList.remove('active-feature'))
+    featureContent.forEach(fc => fc.classList.remove('active-feature'))
     feature.classList.add('active-feature')
   })
 })
