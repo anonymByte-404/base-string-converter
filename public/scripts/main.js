09fd1172cb1d37ab0a2f1e9aa12fb39b74c610b1
anonymByte-404 <an0nymnx@outlook.com>
2025-01-30 20:41:03 -0500

    style: add parentheses for consistency in main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,9 @@
 const featureContent = document.querySelectorAll('.feature-content')
 
-featureContent.forEach(feature => {
+featureContent.forEach((feature) => {
   feature.addEventListener('mouseover', (event) => {
     event.preventDefault()
-    featureContent.forEach(fc => fc.classList.remove('active-feature'))
+    featureContent.forEach((fc) => fc.classList.remove('active-feature'))
     feature.classList.add('active-feature')
   })
 })
