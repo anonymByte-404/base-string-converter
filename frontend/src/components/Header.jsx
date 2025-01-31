808d5606dd1623304b32d5a69daaa1b7061d8cb5
anonymByte-404 <an0nymnx@outlook.com>
2025-01-31 11:37:37 -0500

    feat: add Header component with navigation links

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Base String Converter</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/features">Key Features</Link>
+        </li>
+        <li>
+          <Link to="/how-to-use">How to use</Link>
+        </li>
+      </ul>
+    </header>
+  )
+}
