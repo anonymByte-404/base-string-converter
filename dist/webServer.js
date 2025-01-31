96bbe729f2ec79a027d22b129645600ce8f3207a
anonymByte-404 <an0nymnx@outlook.com>
2025-01-31 11:25:59 -0500

    chore: refactor webServer from TypeScript to JavaScript

--- dist/webServer.js
+++ dist/webServer.js
@@ -2,9 +2,17 @@ import express from 'express'
 import path from 'path'
 const app = express()
 const port = 3000
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../frontend/public')))
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'))
+  res.sendFile(path.join(__dirname, '../frontend/public/index.html'), (err) => {
+    if (err) {
+      res.status(500).send('Error loading the page')
+      console.error('Error serving the page:', err)
+    }
+  })
+})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/public/index.html'))
 })
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)
