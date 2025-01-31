f784d68bb7fe58ab7a4604f9d12705d840b63188
anonymByte-404 <an0nymnx@outlook.com>
2025-01-30 20:43:26 -0500

    chore: build webServer.ts to webServer.js

--- dist/webServer.js
+++ dist/webServer.js
@@ -6,12 +6,6 @@ app.use(express.static(path.join(__dirname, '../public')))
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'))
 })
-app.get('/history', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/history.html'))
-})
-app.get('/convert', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/convert.html'))
-})
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)
 })
