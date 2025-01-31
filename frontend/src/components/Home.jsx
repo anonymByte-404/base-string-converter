9a8afbf9a4cc0daa8cbe50ffc79dcb614155dbe5
anonymByte-404 <an0nymnx@outlook.com>
2025-01-31 11:38:56 -0500

    feat: add Home component with welcome message and call-to-action button

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <section id="home" className="welcome-screen">
+      <h2>Welcome to Base String Converter</h2>
+      <p>
+        Experience Effortless and Precise Conversions:
+        <br />
+        Transform Plain Text to Any Base, Convert Between Different Bases, and
+        Easily Return to Plain Text with{' '}
+        <span className="highlight">Base String Converter</span>
+      </p>
+      <button className="ws-btn">Convert Now!</button>
+    </section>
+  )
+}
