05c48dac5526a7483896eb714414708997672909
anonymByte-404 <an0nymnx@outlook.com>
2025-02-03 20:07:08 -0500

    feat: add scroll functionality and update content in Home component

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
 
 export default function Home() {
+  const scrollToNextSection = () => {
+    document.getElementById("key-features")?.scrollIntoView({ behavior: "smooth" })
+  }
   return (
     <section id="home" className="welcome-screen">
-      <h2>Welcome to Base String Converter</h2>
+      <h2>
+        Seamless Base Conversion for Developers & Learners
+      </h2>
       <p>
-        Experience Effortless and Precise Conversions:
-        <br />
-        Transform Plain Text to Any Base, Convert Between Different Bases, and
-        Easily Return to Plain Text with{' '}
-        <span className="highlight">Base String Converter</span>
+        Convert numbers between binary, octal, decimal, and hexadecimal effortlessly. Whether you're coding or studying number systems, our<br />
+        tool ensures fast and accurate conversions in real time.
       </p>
       <button className="ws-btn">Convert Now!</button>
+      <div className="scroll-down" onClick={scrollToNextSection}>
+        <svg
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="SVGRepo_iconCarrier">
+            <path
+              d="M12 4V20M12 20L8 16M12 20L16 16"
+              stroke="#ffffff"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </g>
+        </svg>
+      </div>
     </section>
   )
 }
